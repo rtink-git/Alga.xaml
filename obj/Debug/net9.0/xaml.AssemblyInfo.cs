e65ad1339bf79c7ff9c77e28174beb3e079e1b48
rtink-git <129110347+rtink-git@users.noreply.github.com>
2026-03-25 18:59:42 +0500

    Release 2.0.4

--- obj/Debug/net9.0/xaml.AssemblyInfo.cs
+++ obj/Debug/net9.0/xaml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xaml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.3.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.3+4f2c20e6ce3e75b6d49f2a4771348ff1be13ad43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.3+c5fe66a33cc424d6d41e12e39322655fd719d522")]
 [assembly: System.Reflection.AssemblyProductAttribute("xaml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xaml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.3.0")]
